13ffece7bd1cb94edd877d78c7fe4534510e0724
Annie <overheads165@gmail.com>
2023-01-22 14:30:50 +0100

    list button

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import products from './products';
+import './App.css';
+
+const IconSwitch = () => {
+  return (
+<span class="material-icons">
+list
+</span>);
+}
+
+function App() {
+  return (
+<IconSwitch/>
+  );
+}
+
+export default App;
